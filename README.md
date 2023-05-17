9e7884f8ca59165ce3c3f495dcefd3dc6e2e7dd4
Mike Lynch <mike.lynch@ca.ey.com>
2023-05-17 16:20:47 -0400

    feat: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Global Atlantic Semantic UI Styles
+
+This is a collection of CSS styles used to override the Semantic UI styles in the Global Atlantic portal.
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// styles
+import 'semantic-ui-css/semantic.css';
+import './ga.css';
+
+// component
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
