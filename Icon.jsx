a801a05e83eb3eda1feaaf3499a8b38f9829661a
Olympia Nassis <41908562+OLyMPiaNAssIS@users.noreply.github.com>
2023-07-14 09:32:11 -0500

    Add files via upload
    the changes made to the code

---
+++ Icon.jsx
@@ -0,0 +1,5 @@
+import { IconSegment } from './Icon/Icon';
+
+<div className={styles.section}>
+  <IconSegment />
+</div>
---
+++ Theme.jsx
@@ -0,0 +1,10 @@
+import { Icon, Header, Form, Segment } from 'semantic-ui-react';
+
+<Segment>
+  <Header as='h2' dividing>
+    Icon
+  </Header>
+  
+    <Icon style = "icon.green" name="users" />
+  ...
+</Segment>
